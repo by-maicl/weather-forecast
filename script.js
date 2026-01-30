6865c03340adac7257003e41ed8a16e58776d969
by-maicl <grabmiho@gmail.com>
2026-01-30 12:25:39 +0200

    Added icons

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const tempFeels = document.getElementById('tempFeels')
 const weather = document.getElementById('weather')
 const loadingIcon = document.getElementById('loadingIcon')
 const prevText = document.getElementById('prevText')
+const weatherIcon = document.getElementById('weatherIcon')
 
 async function fetchData(city) {
     try {
@@ -47,13 +48,41 @@ async function fetchData(city) {
 
         let weatherInfo = data.weather[0].description
         weather.textContent = weatherInfo.replace(weatherInfo[0], weatherInfo[0].toUpperCase())
+
+        if (data.weather[0].icon.at(-1) === 'n') {
+            weatherIcon.src = `icons/weather/${getIcon(data.weather[0].id)}_n.svg`
+        } else {
+            weatherIcon.src = `icons/weather/${getIcon(data.weather[0].id)}.svg`
+        }
+        
     } catch (error) {
         loadingIcon.style.display = 'none'
         prevText.style.display = 'inline'
 
-        prevText.style.color = '#8f0000'
         prevText.textContent = 'Помилка: ' + error
         console.error('Помилка:', error)
+    }
+}
+
+function getIcon(code) {
+    if (code >= 200 && code <= 232) {
+        return 'Thunderstorm'
+    } else if ((code >= 300 && code <= 321)
+            || (code >= 520 && code <= 531)) {
+        return 'Drizzle'
+    }
+    else if (code >= 500 && code <= 504) {
+        return 'Rain'
+    } else if (code === 511 || (code >= 600 && code <= 622)) {
+        return 'Snow'
+    } else if (code >= 701 && code <= 781) {
+        return 'Fog'
+    } else if (code === 800) {
+        return 'Clear'
+    } else if (code === 801) {
+        return 'Clouds'
+    } else if (code >= 802 && code <= 804) {
+        return 'Clouds_full'
     }
 }
 
@@ -63,6 +92,12 @@ btnCity.addEventListener("click", () => {
     if (inputCity === '') {
         return
     }
+
+    temp.textContent = ''
+    tempFeels.textContent = ''
+    weather.textContent = ''
+    cityHeader.innerHTML = ''
+    weatherIcon.src = ''
 
     prevText.style.display = 'none'
     loadingIcon.style.display = 'inline'
